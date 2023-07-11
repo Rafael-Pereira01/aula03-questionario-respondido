9e79557faa6f33d2b606dc6fce0666479f986948
Rafael Pereira da Silva <131472213+Rafael-Pereira01@users.noreply.github.com>
2023-07-11 10:57:30 -0300

    Exercício 9 - ex09.dart

---
+++ ex09.dart
@@ -0,0 +1,12 @@
+/// Exercício 09 - Números Primos:
+/// Escreva uma função que utilize um laço de repetição para imprimir
+/// os números primos de 1 a 100.
+
+void main() {
+  for (int i = 0; i <= 100; ++i) {
+    bool isPrime = true;
+    if (i == 1) {
+      print("$i");
+    }
+  }
+}
